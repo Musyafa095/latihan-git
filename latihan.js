0c3ef2c802d90a793540c825d24f1a378dde0cbc
MusyafaNusiaMulya <musyafa095@gmail.com>
2024-11-16 15:34:53 +0700

    membuat conditional statement

---
+++ latihan.js
@@ -0,0 +1,12 @@
+let nilai = 81;
+if (nilai >= 90) {
+  console.log("A");
+} else if (nilai >= 80) {
+  console.log("B");
+} else if (nilai >= 70) {
+  console.log("C");
+} else if (nilai >= 70) {
+  console.log("D");
+} else {
+  console.log("E");
+}
